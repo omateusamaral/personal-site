b02fbd98d59fd5e19f1e714c1c38f2b1e135b4a0
omateusamaral <mateus.amaral018@gmail.com>
2023-04-18 20:36:25 -0300

    created footer component

---
+++ src/app/Components/Footer.tsx
@@ -0,0 +1,9 @@
+import { SocialMedias } from "./SocialMedias";
+
+export function Footer() {
+  return (
+    <footer className="bg-zinc-950 text-white py-4 w-full">
+      <SocialMedias />
+    </footer>
+  );
+}
