52f006f7b873f9daa68f31f5abcf5ecd9eac18bb
omateusamaral <mateus.amaral018@gmail.com>
2023-04-19 19:18:11 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
+import { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Mateus Amaral - Software engineer",
   description: "Portifolio of Mateus Amaral",
+  icons: {
+    icon: {
+      url: "/favicon.png",
+      type: "image/png",
+    },
+    shortcut: { url: "/favicon.png", type: "image/png" },
+  },
 };
 
 export default function RootLayout({
