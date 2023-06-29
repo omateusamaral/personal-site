4f56142ad75e833f0f9f7dc22b81358f8bdf65bd
omateusamaral <mateus.amaral018@gmail.com>
2023-06-28 22:49:22 -0300

    log click

--- src/app/Components/ChronologyItem.tsx
+++ src/app/Components/ChronologyItem.tsx
@@ -1,4 +1,6 @@
+import { getAnalytics, logEvent } from "firebase/analytics";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { app } from "../firebase.config";
 
 export interface ChronologyItemProps {
   title: string;
@@ -12,6 +14,14 @@ export function ChronologyItem({
   link,
   description,
 }: ChronologyItemProps) {
+  function handleClickLearnMore(link: string, title: string) {
+    if (typeof window !== "undefined") {
+      logEvent(getAnalytics(app), "view_item_list", {
+        item_list_id: link,
+        item_list_name: title,
+      });
+    }
+  }
   return (
     <li className="mb-10 ml-4">
       <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
@@ -30,6 +40,7 @@ export function ChronologyItem({
           className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
           target="_blank"
           rel="noreferrer"
+          onClick={() => handleClickLearnMore(link, title)}
         >
           Learn more <AiOutlineArrowRight className="ml-2" size={14} />
         </a>
