0235b423e0998d02ee42638bfd2548087a2e11d3
omateusamaral <mateus.amaral018@gmail.com>
2023-07-13 19:48:11 -0300

    fix: layout

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
             </a>
           </div>
           <div>
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <ul className="flex flex-row list-none">
               <li className="nav-item">
                 <a
                   className="px-3 py-2 flex items-center text-lg uppercase font-bold leading-snug text-white hover:opacity-75 cursor-pointer"
