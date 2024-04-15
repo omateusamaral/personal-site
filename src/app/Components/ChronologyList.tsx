98cad209feaab0c37fd4d87b023a30b3776aef35
omateusamaral <mateus.amaral018@gmail.com>
2024-04-15 18:50:36 -0300

    feat: new item

--- src/app/Components/ChronologyList.tsx
+++ src/app/Components/ChronologyList.tsx
@@ -1,19 +1,19 @@
-import { useTranslation } from "react-i18next";
-import { chronologies, chronologiesPT } from "../chronologies.json";
-import { ChronologyItem } from "./ChronologyItem";
-import { useMemo } from "react";
+import { useTranslation } from 'react-i18next';
+import { chronologies, chronologiesPT } from '../chronologies.json';
+import { ChronologyItem } from './ChronologyItem';
+import { useMemo } from 'react';
 
 interface ChronologyListProps {
   itemsLength: number;
 }
-export function ChronologyList({ itemsLength }: ChronologyListProps) {
+export function ChronologyList({ itemsLength }: Readonly<ChronologyListProps>) {
   const { i18n } = useTranslation();
 
   const currentChronologies =
-    i18n.language === "pt" ? chronologiesPT : chronologies;
+    i18n.language === 'pt' ? chronologiesPT : chronologies;
   const ChronologyListMemoized = useMemo(
     () => (
-      <ol className="relative border-l border-gray-200 dark:border-gray-700">
+      <ol className='relative border-l border-gray-200 dark:border-gray-700'>
         {currentChronologies.slice(0, itemsLength).map((chronology) => (
           <ChronologyItem
             key={chronology.id}
